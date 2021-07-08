54052e210b173c4d06c6a0581f3f9735d8edadcc
Carlos Prosdoskimi <henriqueprosdoskimi@hotmail.com>
2021-07-08 00:47:12 -0300

    adding level06 exercises

--- 06_Day_Loops/06_day_starter/exercises/level-01.js
+++ 06_Day_Loops/06_day_starter/exercises/level-01.js
@@ -1,30 +1,4 @@
 // Exercises: Level 1
-const countries = [
-  'Albania',
-  'Bolivia',
-  'Canada',
-  'Denmark',
-  'Ethiopia',
-  'Finland',
-  'Germany',
-  'Hungary',
-  'Ireland',
-  'Japan',
-  'Kenya'
-]
-
-const webTechs = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'React',
-  'Redux',
-  'Node',
-  'MongoDB'
-]
-
-// const mernStack = ['MongoDB', 'Express', 'React', 'Node']
-
 // 1. Iterate 0 to 10 using for loop, do the same using while and do while loop
 
 for(let i =0;i<=10 ;i++){
---
+++ 06_Day_Loops/06_day_starter/exercises/level-03.js
@@ -0,0 +1,36 @@
+// Exercises: Level 3
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+// const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+// 1. Copy countries array(Avoid mutation)
+// 2. Arrays are mutable. Create a copy of array which does not modify the original. Sort the copied array and store in a variable sortedCountries
+// 3. Sort the webTechs array and mernStack array
+// 4. Extract all the countries contain the word 'land' from the countries array and print it as array
+// 5. Find the country containing the hightest number of characters in the countries array
+// 6. Extract all the countries contain the word 'land' from the countries array and print it as array
+// 7. Extract all the countries containing only four characters from the countries array and print it as array
+// 8. Extract all the countries containing two or more words from the countries array and print it as array
+// 9. Reverse the countries array and capitalize each country and stored it as an array
